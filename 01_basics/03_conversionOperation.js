6d621961ddeac451dde9b843e40d24668f6f93d8
Saiabhishek12345 <saiabhishekturke@gmail.com>
2024-08-06 12:34:40 +0000

    conversionOperator

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,87 @@
+let score = "saiabhsihek" ;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueIntNumber= Number(score)
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+
+//"33"=>33
+//"33abc" =>NaN
+// true =>1; false=>0
+
+let isLoggedIn= 123;
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// "" => false
+// "saiabhishek" =>true
+
+let someNumber=33
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*****************operation **********/
+
+let value =3
+let negValue= -value
+console.log(negValue);
+
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2**3); //it 2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "saiabhishek"
+let str2 = "turke"
+
+let str3= str1 + str2 ;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1=num2=mum3=2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
