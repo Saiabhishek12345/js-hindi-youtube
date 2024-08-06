f4092536737e4819fd740137d085e90a35e761af
Saiabhishek12345 <saiabhishekturke@gmail.com>
2024-08-06 10:44:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+console.log(3+
+    3
+);// code readability should high 
+
+console.log("Saiabshihek")
+
+let name="saiabhishek"
+let age =18
+let isLoggedIn = false 
+
+
+// number => 2 to power 53
+// bigint 
+// string=> ""
+// boolean=> true/false
+// null=> standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
