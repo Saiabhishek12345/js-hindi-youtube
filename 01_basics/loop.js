581d2da8300ffac3d0fc53321485babc1def4e2c
Saiabhishek12345 <saiabhishekturke@gmail.com>
2024-08-16 12:40:36 +0000

    loop

---
+++ 01_basics/loop.js
@@ -0,0 +1,7 @@
+let gameNumber=25;
+
+let userNumber=prompt("guess the gameNumber :")
+while(gameNumber!=userNumber){
+    userNumber=prompt("you entered wrong number.Guess again")
+}
+console.log("congratulation , you enetered a right number ");
