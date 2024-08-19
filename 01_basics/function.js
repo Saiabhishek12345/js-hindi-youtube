cbcee51c6302d1fbc8570184c67c873c8ad71d9d
Saiabhishek12345 <saiabhishekturke@gmail.com>
2024-08-19 16:41:40 +0000

    function

--- 01_basics/function.js
+++ 01_basics/function.js
@@ -17,15 +17,52 @@ console.log(val);*/
     
 }*/
 
-function countVowels(str){
-    let count=0;
-    for(const char of str){
-       if(char==='a' || char==='e' || char==='i' || char==='o'){
-        count++;
-       }
+// function countVowels(str){
+//     let count=0;
+//     for(const char of str){
+//        if(char==='a' || char==='e' || char==='i' || char==='o'){
+//         count++;
+//        }
         
-    }
-    console.log(count);
+//     }
+//     console.log(count);
     
-}
+// }
+
+/* let arr=["pune","mumbai","delhi"];
+
+arr.forEach((val,idx,arr)=>{
+    console.log(val.toUpperCase(),idx,arr);
+    
+
+});
+let brr=[1,2,3,4,5];
+brr.forEach((val)=>{
+    console.log(val*val);
+    
+});
+
+let num=[34,56,78];
+
+let newArr=num.map((val)=>{
+    return val*val;
+});
+console.log(newArr);*/
+
+/*let arr=[1,2,3,4,5];
+
+let newArr=arr.filter((val)=>{
+    return val%2===0;
+})
+
+console.log(newArr);*/
+
+let arr=[5,6,2,1,7];
+
+let maxArr=arr.reduce((prev,curr)=>{
+    return prev > curr ? prev : curr;
+});
+
+console.log(maxArr);
+
 
