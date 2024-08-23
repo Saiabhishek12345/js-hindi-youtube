5b0c62b5586d9824d706b83b7e2cda9a5bfbd64f
Saiabhishek12345 <saiabhishekturke@gmail.com>
2024-08-23 12:34:43 +0000

    dom

---
+++ 01_basics/dom.js
@@ -0,0 +1,29 @@
+/*let heading=document.getElementById("myId");
+console.log(heading);
+let heading2=document.getElementsByClassName("heading-class");
+console.dir(heading2);
+console.log(heading2);
+let parahs=document.getElementsByTagName("p");
+console.dir(parahs);
+
+let firstEle=document.querySelector("p");
+console.dir(firstEle);
+let allEle=document.querySelectorAll("p");
+console.dir(allEle);
+
+let div=document.querySelector("div");
+console.dir(div);
+
+let heading=document.querySelector("h1");*/
+
+/*let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText=h2.innerText + " from Apna college student"*/
+
+let divs= document.querySelectorAll(".box");
+console.dir(divs[2]);
+
+for(div of divs){
+    console.log(div.innerText);
+}
