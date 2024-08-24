3912940e5fb081d805257af12e6b5d4d2c1cf59e
Saiabhishek12345 <saiabhishekturke@gmail.com>
2024-08-24 11:50:49 +0000

    dom2

--- 01_basics/dom.js
+++ 01_basics/dom.js
@@ -21,9 +21,36 @@ console.dir(h2.innerText);
 
 h2.innerText=h2.innerText + " from Apna college student"*/
 
-let divs= document.querySelectorAll(".box");
+/*let divs= document.querySelectorAll(".box");
 console.dir(divs[2]);
 
 for(div of divs){
     console.log(div.innerText);
-}
+}*/
+/* let div=document.querySelector("div");
+div.style.backgroundColor="green";
+div.style.fontSize="26px";
+div.innerText="hello";
+div.style.visibility="hidden";*/
+
+/*let newBtn=document.createElement("button");
+newBtn.innerText="click me!";
+console.log(newBtn);
+let p=document.querySelector("p");
+p.after(newBtn);*/
+
+/*let newHeading=document.createElement("h1");
+newHeading.innerHTML="<i>Hi. I am new!</i>";
+document.querySelector("body").prepend(newHeading);
+
+let para=document.querySelector("p");
+para.remove();*/
+
+/*let newBtn=document.createElement("button");
+newBtn.innerText="CLick me!";
+newBtn.style.backgroundColor="red";
+newBtn.style.color="white";
+let body=document.querySelector("body");
+body.prepend(newBtn);*/
+let para=document.querySelector("p");
+
